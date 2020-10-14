cdc8d09d0b8d441994a437e696b6a1a86a8a7446
xSkjj <xSkj@mail.de>
2020-10-14 08:52:47 +0200

    ooops

--- modules/functions/spin.py
+++ modules/functions/spin.py
@@ -22,7 +22,7 @@ def trySpin():
 
 
 def go(amt):
-    symbols = ["0", "1", "2", "3", "4", "5", "6", "7"]#, "8", "9", "0", "!", "$", "%", "&", "?", "#"]
+    symbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "$", "%", "&", "?", "#"]
     gui.output["text"] = "spinning..."
 
     gui.spinBtn["state"] = "disabled" # lock spin button
