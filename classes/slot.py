cdcb9eb0edc3ba0360665a25dc36d764c65a436f
xSkjj <xSkj@mail.de>
2020-11-09 14:38:17 +0100

    Update slot.py

--- classes/slot.py
+++ classes/slot.py
@@ -1,18 +1,17 @@
 from random import randint
-from modules import bgSlots, slotFont, symColor, symbols
+from modules import slotFont, symColor, symbols
 
 class Slot:
     def __init__(self, canv, nth):
         self.canv = canv
-        self.bgColor = bgSlots
         self.slotFont = slotFont
         self.symColor = symColor
-        self.bg = canv.create_rectangle(nth*100+2, 2,
-                                        nth*100+98, 98,
-                                        width = 0,
-                                        fill    = self.bgColor)
         self.sym = canv.create_text(nth*100+50, 50,
                                     text = symbols[randint(0, len(symbols) - 1)],
                                     font = (slotFont, 48, "bold"),
                                     fill = self.symColor,
                                     tags = f"sym{nth}")
+        self.bg = canv.create_rectangle(nth*100+1, 1,
+                                        nth*100+99, 99,
+                                        width = 2,
+                                        outline = "#692020")
