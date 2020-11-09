6c55073de6c10bfd72bc9b3468aa703f9e4179b2
xSkjj <xSkj@mail.de>
2020-11-09 14:04:28 +0100

    this

--- modules/func.py
+++ modules/func.py
@@ -83,8 +83,8 @@ def go(amt):
             else:
                 bal += amt * round((1 / (1 - (1 - 1 / len(symbols)**(win-1))**(slotAmt-1)))/5) # this is a BIG mess
                 gui.output["text"] = f"You spent {amt} and won {amt * round((1 / (1 - (1 - 1 / len(symbols)**(win-1))**(slotAmt-1)))/15)} !"
-        else:                                                            #<┬ this is the only stuff that's not a mess
-            gui.output["text"] = f"You spent {amt} and lost everything." #<┘ maybe...
+        else:
+            gui.output["text"] = f"You spent {amt} and lost everything."
     # ^^^ help me, I'm dying ^^^ --------------------------------------------------------------MESS------------------
 
     gui.balLabel["text"] = f"Balance: {bal}" # update the balance Label with the new balance
