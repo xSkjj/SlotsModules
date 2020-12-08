13a17b9cdaf5687fd97397fd0076dc8cc6b070ca
xSkjj <21961241+xSkjj@users.noreply.github.com>
2020-12-08 07:25:43 +0100

    bigger ni... slot border

--- classes/slot.py
+++ classes/slot.py
@@ -3,9 +3,9 @@ class Slot:
         self.canvas = canvas
         self.symbol = self.canvas.create_text(nth * 120 + 60, 60,
                                               font=("Consolas", 56))
-        self.bg = self.canvas.create_rectangle(nth * 120 + 2, 2,
-                                               nth * 120 + 118, 118,
-                                               width=4,
+        self.bg = self.canvas.create_rectangle(nth * 120 + 12, 12,
+                                               nth * 120 + 108, 108,
+                                               width=24,
                                                outline="#692020")
 
     def set(self, to_set, **kwargs):
