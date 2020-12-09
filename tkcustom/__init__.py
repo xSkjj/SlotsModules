751d2b0948de29a72954fe70ac351e91adde9432
xSkjj <21961241+xSkjj@users.noreply.github.com>
2020-12-09 16:30:10 +0100

    bread in french
    ...happened today

--- tkcustom/__init__.py
+++ tkcustom/__init__.py
@@ -1,45 +1,81 @@
 import tkinter as tk
+from tkinter import colorchooser
 from misc import utility, bgColor
 
 
 class Button(tk.Button):
+    """
+    Custom tkinter Button Widget
+    """
     def __init__(self, master, color, **kwargs):
-        self._diff = -80 if utility.is_bright(color) else 80
-        self.bg = color
-        self.fg = utility.set_brightness(color, self._diff)
-        self.abg = utility.set_brightness(color, -16)
-        self.afg = utility.set_brightness(color, self._diff - 16)
+        self._init_colors(color)
         super().__init__(master,
-                         bg=color,
+                         bg=self.bg,
                          fg=self.fg,
                          activebackground=self.abg,
                          activeforeground=self.afg,
                          relief="flat",
                          **kwargs)
 
-        self.bind("<Enter>", self.on_enter)
-        self.bind("<Leave>", self.on_leave)
+        self.bind("<Enter>", self._on_enter)
+        self.bind("<Leave>", self._on_leave)
 
-    def on_enter(self, _):
+    def _init_colors(self, color):
+        """
+        forbidden, unless...
+        """
+        _diff = -80 if utility.is_bright(color) else 80
+        self.bg = color
+        self.fg = utility.set_brightness(color, _diff)
+        self.abg = utility.set_brightness(color, -16)
+        self.afg = utility.set_brightness(color, _diff - 16)
+
+    def _on_enter(self, _):
+        """
+        you shall not use this!
+        """
         if self["state"] != "disabled":
             self["bg"] = utility.set_brightness(self.bg, 20)
             self["fg"] = utility.set_brightness(self.fg, 20)
 
-    def on_leave(self, _):
+    def _on_leave(self, _):
+        """
+        access denied
+        """
         self["bg"] = self.bg
         self["fg"] = self.fg
 
+    def change_color(self, color):
+        """
+        Change the button color,
+        along with the following variables:
+        .bg, .fg, .abg, .afg
+        """
+        self._init_colors(color)
+        self.config(bg=self.bg,
+                    fg=self.fg,
+                    activebackground=self.abg,
+                    activeforeground=self.afg)
 
-class ColorPicker(Button):
-    def __init__(self, master, color, setting, **kwargs):
+
+class ColorChooser(Button):
+    def __init__(self, master, color, callback=None, **kwargs):
         super().__init__(master, color,
                          text="pick",
                          font="Consolas 12 bold",
-                         command=lambda: self.change_setting(setting),
+                         command=lambda: self.askcolor(callback),
                          **kwargs)
 
-    def change_setting(self, setting):
-        print(setting)
+    def askcolor(self, callback):
+        """
+        Opens a color chooser window and
+        changes the button color to the chosen color,
+        along with the following variables:
+        .bg, .fg, .abg, .afg
+        """
+        rgb, color = colorchooser.askcolor()
+        super().change_color(color) if color else None
+        callback() if callback else None
 
 
 class Section(tk.Frame):
